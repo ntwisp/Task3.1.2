6c29bd775dc2ffb18caa8765d67b28214d6fdcac
ntwisp <liangush@yandex.ru>
2024-11-04 00:49:21 +0300

    Убрал гет-методы)

--- src/main/java/ru/itmentor/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/models/User.java
@@ -38,16 +38,6 @@ public class User implements UserDetails {
     }
 
     @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
